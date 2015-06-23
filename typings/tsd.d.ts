ce3ac1f2be908a771b47cc18fbe3ef1e6b5237f3
Sebastian Müller <info@sebastian-mueller.net>
2015-06-23 19:42:10 +0200

    fix(typings): fix path to angular2-boostrap d.ts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,4 +2,4 @@
 /// <reference path="rx/rx-lite.d.ts" />
 /// <reference path="rx/rx.d.ts" />
 /// <reference path="angular2/angular2.d.ts" />
-/// <reference path="../jspm_packages/npm/angular2-bootstrap@dev/typings/angular2-bootstrap.d.ts" />
+/// <reference path="../jspm_packages/npm/angular2-bootstrap@0.2.1/typings/angular2-bootstrap.d.ts" />
